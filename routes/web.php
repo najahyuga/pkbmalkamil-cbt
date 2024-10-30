e5a54f476a190591bbc4d2bbcc8aeb4081112467
Muhammad Najah Yuga Desyalwa <najahyugad@gmail.com>
2024-10-30 20:42:22 +0700

    feat: add new routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\CourseQuestionController;
+use App\Http\Controllers\CourseStudentController;
+use App\Http\Controllers\LearningController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StudentAnswerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +20,26 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('dashboard')->name('dashboard.')->group(function(){
+        Route::resource('courses', CourseController::class)->middleware('role:teacher');
+
+        Route::get('/course/question/create/{course}', [CourseQuestionController::class, 'create'])->middleware('role:teacher')->name('course.create.question');
+        Route::post('/course/question/save/{course}', [CourseQuestionController::class, 'store'])->middleware('role:teacher')->name('course.create.question.store');
+
+        Route::resource('course_questions', CourseQuestionController::class)->middleware('role:teacher');
+
+        Route::get('/course/students/show/{course}', [CourseStudentController::class, 'index'])->middleware('role:teacher')->name('course.course_students.index');
+
+        Route::get('/course/students/create/{course}', [CourseStudentController::class, 'create'])->middleware('role:teacher')->name('course.course_students.create');
+        Route::post('/course/students/save/{course}', [CourseStudentController::class, 'store'])->middleware('role:teacher')->name('course.course_students.store');
+
+        Route::get('/learning/finished/{course}', [LearningController::class, 'learning_finished'])->middleware('role:student')->name('learning.finished.course');
+        Route::get('/learning/raport/{course}', [LearningController::class, 'learning_raport'])->middleware('role:student')->name('learning.raport.course');
+
+        Route::get('/learning', [LearningController::class, 'index'])->middleware('role:student')->name('learning.index');
+        Route::get('/learning/{course}/{question}', [StudentAnswerController::class, 'store'])->middleware('role:student')->name('learning.course.answer.store');
+    });
 });
 
 require __DIR__.'/auth.php';
